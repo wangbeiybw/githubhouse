9ad8a5a2d7282f670861a24be333f0f9f504d6e1
xingyangbeibei <297766956@qq.com>
2020-08-06 16:49:44 +0800

    测试分支

--- myreact/src/production/B.js
+++ myreact/src/production/B.js
@@ -5,7 +5,7 @@ export default class B extends React.Component{
     render(){
         return (
         	<div>
-              <div>test分之分支</div>
+              <div>test分之分支测试333333333333</div>
 
             </div>
         );
