f349100730b4594274d0510fa447dc1a38c55690
xingyangbeibei <297766956@qq.com>
2020-08-06 16:49:23 +0800

    测试分支

--- myreact/src/production/B.js
+++ myreact/src/production/B.js
@@ -5,7 +5,7 @@ export default class B extends React.Component{
     render(){
         return (
         	<div>
-              <div>版本管理器用起来怎么看不到</div>
+              <div>test分之分支</div>
 
             </div>
         );
